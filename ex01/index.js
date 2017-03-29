bebc15540febcdcc116317330eaed6cad191f5ef
Zleub <debray.arnaud@gmail.com>
2017-03-29 18:31:28 +0200

    ex01 test

--- ex01/index.js
+++ ex01/index.js
@@ -9,17 +9,17 @@ Créer le fichier testDay00 dans votre répertoire de rendu.
 Vous devrez faire en sorte que (à part "total 1") :
 
     $> ls -l testDay00
-    total 1
     -r--r-xr-x 1 login wheel 40 Jun 1 23:42 testDay00
 
-Une fois l’exercice résolu, vous exécuterez la commande tar -cf testDay00.tar
-testDay00 pour créer le fichier à rendre.
+Une fois l’exercice résolu, vous exécuterez la commande tar -cf testDay00.tar testDay00 pour créer le fichier à rendre.
+
+"login" et "wheel" seront remplacés respectivement par votre login et votre groupe.
 `;
 
 exports.verify = (args, cb) => {
 	let exec = require('child_process').spawn;
 
-	exec.stdout.on('data', (data) => {
+	exec('ls ' + args[0]).stdout.on('data', (data) => {
 		console.log('stdout: ' + data);
 	});
-}
+};
