94be1ef1f84c998922e420355d959c2c7e2d0d7d
Zleub <debray.arnaud@gmail.com>
2017-03-29 18:15:06 +0200

    format

--- ex00/index.js
+++ ex00/index.js
@@ -1,9 +1,15 @@
-exports.problem = `Enter my fucking name. Should be lowercase.
-You can validate your solution with \`shell-work verify <my-name>\``
+exports.problem = `
+Exercice 00: Verify your work
+
+Files to submit: none
+
+Enter my fucking name. Should be lowercase.
+You can validate your solution with \`shell-work verify '<my-name>'\`
+`;
 
 exports.verify = (args, cb) => {
 	if (args == 'Arnaud Debray')
-		cb(true)
+		cb(true);
 	else
-		cb(false)
-}
+		cb(false);
+};
