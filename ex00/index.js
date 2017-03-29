1565d2f8734ca41e0a84036dde7cad78cc6fc6ec
Zleub <debray.arnaud@gmail.com>
2017-03-29 17:23:46 +0200

    basic ex II

--- ex00/index.js
+++ ex00/index.js
@@ -1,4 +1,9 @@
-exports.problem = 'Enter my fucking name. Should be lowercase.'
+exports.problem = `Enter my fucking name. Should be lowercase.
+You can validate your solution with \`shell-work verify <my-name>\``
 
 exports.verify = (args, cb) => {
+	if (args == 'Arnaud Debray')
+		cb(true)
+	else
+		cb(false)
 }
