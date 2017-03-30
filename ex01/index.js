76ed19ab4ab9c3eeb200c3f3ccd5a065972971ef
Zleub <debray.arnaud@gmail.com>
2017-03-30 14:37:18 +0200

    ex01 test

--- ex01/index.js
+++ ex01/index.js
@@ -3,24 +3,34 @@
 exports.problem = `
 Exercice 01: File Attributes
 
-Files to submit : testDay00.tar
+Files to submit : testDay00
 
-Créer le fichier testDay00 dans votre répertoire de rendu.
-Vous devrez faire en sorte que (à part "total 1") :
+Create the testDay00 in your directory such as:
 
     $> ls -l testDay00
     -r--r-xr-x 1 login wheel 40 Jun 1 23:42 testDay00
 
-Une fois l’exercice résolu, vous exécuterez la commande tar -cf testDay00.tar testDay00 pour créer le fichier à rendre.
+You can validate your solution with \`shell-work verify <your-file>\`.
 
-"login" et "wheel" seront remplacés respectivement par votre login et votre groupe.
+"login" et "wheel" will be respectively replaced by your name and group.
 `;
 
+/*
+ exports.solution = `
+touch -d '2017-03-01 23:42' testDay00
+`
+*/
+
 exports.verify = (args, cb) => {
-	let exec = require('child_process').spawn('ls ', [ argv[0] ]);
+	const exec = require('child_process').spawn('ls', [ '-l', args[0] ]);
 
 	exec.stdout.on('data', (data) => {
-		console.log(`stdout: ${data}`);
+		let res = data.toString();
+
+		console.log('-r--r-xr-x 1 login wheel 40 Jun 1 23:42 testDay00');
+		console.log(res.split(' '));
+
+		cb('-r--r-xr-x 1 login wheel 40 Jun 1 23:42 testDay00' == res);
 	});
 
 	exec.stderr.on('data', (data) => {
