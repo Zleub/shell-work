5f0cd62fba46ae16b8ce6a2598e75a1b441242cc
Zleub <debray.arnaud@gmail.com>
2017-04-01 12:55:24 +0200

    introduction to be written again

---
+++ introduction/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+exports.problem = `Introduction: shell-work.
+
+Welcome to the AWESOME Encloserie's formation.
+The purpose of this training is to initiate you to the arcane of the computer's tools.
+It may seems useless on first glance but a great power requires a tought training, don't you dare forget that.
+
+This first chapter is dedicated to the shell and it's basics utilities. You will learn about files and directories, to be careful about them since it's almost enought to summarize a computer.
+
+`;
+
+exports.verify = (args, cb) => cb(true);
