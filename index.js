d4883e0329c3d735bc817bc007a5fa5ac40cd209
Zleub <debray.arnaud@gmail.com>
2017-04-01 12:53:32 +0200

    class structure

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-#!/usr/bin/env node --trace-warnings
+#!/usr/bin/env node
 
 var adventure = require('adventure');
 var shop = adventure('shell-work');
 
-shop.add('Learn to verify your work', () => require('./learntoverify') );
-shop.add('File Attributes', () => require('./fileattributes') );
-shop.add('File Attributes II', () => require('./fileattributes2') );
+var shops = [
+	[ 'Introduction', './introduction' ],
+	// [ 'Learn to verify your work', './learntoverify' ],
+	[ 'Git', './git' ],
+	// [ 'Git II', './git2' ],
+	[ 'File Attributes', './fileattributes' ],
+	[ 'File Attributes II', './fileattributes2' ],
+	[ 'Print Groups', './print_groups' ]
+];
+
+shops.forEach( (e, i) => {
+	if (i < 10)
+		shop.add(`ex0${i}: ${e[0]}`, () => require(e[1]));
+	else
+		shop.add(`ex${i}: ${e[0]}`, () => require(e[1]));
+});
 
 shop.execute(process.argv.slice(2));
