4faafcb8fb1522bf79f6ca94e52cca996dde87b9
Zleub <debray.arnaud@gmail.com>
2017-04-21 23:33:27 +0200

    workshopper-adventure init style

--- index.js
+++ index.js
@@ -1,43 +1,18 @@
 #!/usr/bin/env node
 
-// var adventure = require('adventure');
-// var shop = adventure('shell-work');
-//
-// var shops = [
-// 	[ 'Introduction', './introduction' ],
-// 	// [ 'Learn to verify your work', './learntoverify' ],
-// 	[ 'Git', './git' ],
-// 	// [ 'Git II', './git2' ],
-// 	[ 'File Attributes', './fileattributes' ],
-// 	[ 'File Attributes II', './fileattributes2' ],
-// 	[ 'Print Groups', './print_groups' ]
-// ];
-//
-// shops.forEach( (e, i) => {
-// 	if (i < 10)
-// 		shop.add(`ex0${i}: ${e[0]}`, () => require(e[1]));
-// 	else
-// 		shop.add(`ex${i}: ${e[0]}`, () => require(e[1]));
-// });
-//
-// shop.execute(process.argv.slice(2));
-
-console.log( process.cwd() )
-console.log( require('fs').readdirSync('./') )
-
 var jsing = require('workshopper-adventure')({
 	appDir: __dirname,
 	languages: ['en'],
 	header: require('workshopper-adventure/default/header'),
-	footer: require('./lib/footer')
+	footer: require('./lib/footer'),
+	help: require('./lib/help.js')
 });
 
 jsing.addAll(require('./menu.json').map(function (problem) {
-	console.log('problem: ', problem)
 	return {
 		name: problem,
 		fn: function () {
-			var p = problem.toLowerCase().replace(/\s/g, '-');
+			var p = problem.toLowerCase().replace(/\s/g, '_');
 			var dir = require('path').join(__dirname, 'problems', p);
 			return require(dir);
 		}
