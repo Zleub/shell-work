5fd26d09a727e2532d2fd4d41481a504bb64915d
Zleub <debray.arnaud@gmail.com>
2017-04-22 13:24:28 +0200

    working on a way to verify a shell exercice

--- problems/file_attributes/index.js
+++ problems/file_attributes/index.js
@@ -1,4 +1,57 @@
-module.exports = require("../../lib/problem")(__dirname)
+var getFile = require('../../lib/get-file');
+var w = require("../../lib/problem")(__dirname)
+var diff = require("diff");
+
+function generateDiff(solution, attempt) {
+
+  var parts = diff.diffChars(solution, attempt);
+
+  var result = "";
+
+  parts.forEach(function(part) {
+
+    if(part.added) {
+      result += part.value["bgRed"];
+    } else if(part.removed) {
+      result += part.value["bgGreen"];
+    } else {
+      result += part.value;
+    }
+
+  });
+
+  return result;
+
+}
+
+w.verify = function (args, cb) {
+    console.log(args)
+	var stdout = require('child_process').execSync('ls -l ' + args[0]).toString()
+	var res = stdout.toString().match(/[\w-:]*/g).filter( (e) => e != '' );
+
+    var message = getFile(w.troubleshootingPath);
+    var solution = '-r--r-xr-x 1 login wheel 40 Mar 1 23:42 testDay00'
+
+    message = message.replace(/%solution%/g, solution);
+    message = message.replace(/%attempt%/g, stdout);
+    message = message.replace(/%diff%/g, generateDiff(solution, stdout));
+    message = message.replace(/%filename%/g, args[0]);
+
+
+    w.fail = [
+      {text: message, type: 'md' }
+    ]
+
+
+	cb('-r--r-xr-x 1 login wheel 40 Mar 1 23:42 testDay00'.split(' ').reduce( (p, e, i) => {
+		if (e == 'login' || e == 'wheel')
+			return p;
+		else
+			return p && e == res[i];
+	}, true));
+}
+
+module.exports = w
 
 // "use strict";
 //
